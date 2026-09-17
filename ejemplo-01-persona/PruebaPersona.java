2655c3595c73097eb372eed3c720756b63fde24e
Jesus Daniel Gazabon Beltran <jgazabonb1@unicartagena.edu.co>
2026-09-17 13:21:12 -0500

    (ejemplo1): constructor por defecto clase Persona

---
+++ ejemplo-01-persona/PruebaPersona.java
@@ -0,0 +1,6 @@
+public class PruebaPersona {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        System.out.println("Nombre: " + persona1.nombre);
+    }
+}
---
+++ ejemplo-01-persona/persona.java
@@ -0,0 +1,8 @@
+public class Persona {
+    String nombre;
+
+    // Constructor
+    public Persona() {
+        nombre = "Sin nombre";
+    }
+}
