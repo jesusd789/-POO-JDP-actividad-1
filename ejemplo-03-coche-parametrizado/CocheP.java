03730604070bf804a3c1e99522eee54b7c2255be
Jesus Daniel Gazabon Beltran <jgazabonb1@unicartagena.edu.co>
2026-09-17 14:06:28 -0500

    (ejemplo03): constructor parametrizado clase Coche

---
+++ ejemplo-03-coche-parametrizado/CocheP.java
@@ -0,0 +1,8 @@
+public class CocheP {
+    String marca;
+
+    // Constructor parametrizado
+    public CocheP(String marca) {
+        this.marca = marca;
+    }
+}
---
+++ ejemplo-03-coche-parametrizado/PruebaCocheP.java
@@ -0,0 +1,6 @@
+public class PruebaCocheP {
+    public static void main(String[] args) {
+        CocheP coche1 = new CocheP("Toyota");
+        System.out.println("Marca: " + coche1.marca);
+    }
+}
