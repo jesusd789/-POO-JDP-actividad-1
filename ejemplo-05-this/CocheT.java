ae0f554469eb59bae6afc773575fa40cc635d460
Jesus Daniel Gazabon Beltran <jgazabonb1@unicartagena.edu.co>
2026-09-17 14:15:28 -0500

    (ejemplo05) : uso de this entre constructores

---
+++ ejemplo-05-this/CocheT.java
@@ -0,0 +1,15 @@
+public class CocheT {
+    String marca;
+    String modelo;
+
+    // Constructor con un parámetro
+    public Coche(String marca) {
+        this.marca = marca;
+    }
+
+    // Llamada al constructor anterior usando this()
+    public Coche(String marca, String modelo) {
+        this(marca); // Llama al constructor de un solo parámetro
+        this.modelo = modelo;
+    }
+}
---
+++ ejemplo-05-this/PruebaCocheT.java
@@ -0,0 +1,6 @@
+public class PruebaCocheT {
+    public static void main(String[] args) {
+        Coche c1 = new Coche("Chevrolet", "Spark");
+        System.out.println("Marca: " + c1.marca + " Modelo: " + c1.modelo);
+    }
+}
