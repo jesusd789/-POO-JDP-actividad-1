fd1ab4be8c11cd2aa1471d97b33b6c20d576a260
Jesus Daniel Gazabon Beltran <jgazabonb1@unicartagena.edu.co>
2026-09-17 14:26:24 -0500

    (ejemplo06) : uso de super() con herencia de vehiculos

---
+++ ejemplo-06-super-herencia/CocheH.java
@@ -0,0 +1,8 @@
+public class CocheH extends Vehiculo {
+    String marca;
+
+    public CocheH(String tipo, String marca) {
+        super(tipo);
+        this.marca = marca;
+    }
+}
---
+++ ejemplo-06-super-herencia/PruebaCocheH.java
@@ -0,0 +1,6 @@
+public class PruebaCocheH {
+    public static void main(String[] args) {
+        CocheH c1 = new CocheH("Automóvil", "Ford");
+        System.out.println("Tipo: " + c1.tipo + " Marca: " + c1.marca);
+    }
+}
